21e80181a2c6cea06e01109b74f7baf5b4d717a9
selva <maildemotesting2001@gmail.com>
2025-07-02 22:22:46 +0530

    Basic folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'smart-drive-project';
+  title = 'your-app-name'; // you can set this as needed
 }
---
+++ src/app/core/constant/APIConstants.ts
@@ -0,0 +1,8 @@
+export const APIConstants = {
+  BASE_URL: '',
+  AUTH: {
+    LOGIN: '/auth/login',
+    LOGOUT: '/auth/logout',
+    REGISTER: '/auth/register',
+  }
+}
